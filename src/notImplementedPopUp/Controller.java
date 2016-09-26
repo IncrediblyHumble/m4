e75a17b63e3508af4104fb0c086ea1c692fa38d7
Maddie Ravichandran <ramaddie07@gatech.edu>
2016-09-25 21:01:08 -0400

    did not implemented error

--- src/notImplementedPopUp/Controller.java
+++ src/notImplementedPopUp/Controller.java
@@ -1,5 +1,27 @@
 package notImplementedPopUp;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.Window;
 
-public class Controller {
-    
+
+    public class Controller {;
+
+        /**
+         * notImplemented takes in a screen and displays error message alerting that
+         * this feature hasn't been implemented yet
+         * @param owner screen that will display the error
+         */
+        @FXML
+        public void notImplemented(Window owner) {
+            // Show the error message for unimplemented methods
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.initOwner(owner);
+            alert.setTitle("Unimplemented");
+            alert.setHeaderText("Unimplemented Feature");
+            alert.setContentText("Sorry, this feature has not been implemented yet!");
+
+            alert.showAndWait();
+
+        }
+    }
 }
