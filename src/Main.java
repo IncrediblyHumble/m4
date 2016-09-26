fe6adc7fa1c4cb475d4cb4601402fdec30a93f2c
Noam Lerner <noaml1295@gmail.com>
2016-09-25 21:25:03 -0400

    git ignore and case fix

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("welcomescreen/welcome.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("welcomeScreen/welcome.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 700, 500));
         primaryStage.show();
